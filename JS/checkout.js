ab5239f985ae1f4c156c9228e75c8ccbaf22dc51
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-12-14 12:12:14 +0200

    'main'

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,4 +1,30 @@
+// Retrieve an write Local Storage
+if (ItemNo == "" || Item == "" || RefNo == "" || Price == "" || Qty == ""){
+}else{
+    if(products == []){
+        const list = document.querySelector("#row");
+        const row = document.createElement("tr");
 
+        row.innerHTML = `
+        <td>${ItemNo}</td>
+        <td>${Item}</td>
+        <td>${Price}</td>
+        <td>${Qty}</td>
+        <td>${Total}</td>
+        <td>
+        <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+        <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
+        `;
+        list.appendChild(row);
+    }else{
+        chosen.children[0].textContent = ItemNo;
+        chosen.children[1].textContent = Item;
+        chosen.children[2].textContent = Price;
+        chosen.children[3].textContent = Qty;
+        chosen.children[4].textContent = Total;
+    }
+    updatePage();
+};
 // Alerts //
 
 //Data-logging removal //
--- JS/product.js
+++ JS/product.js
@@ -66,119 +66,126 @@ console.log(JSON.parse(localStorage.getItem('products'))
 );
 localStorage.getItem('products');
 // Retrieve an write Local Storage
-// function retrieve(){ //retrieves items in the localStorage
-//     console.log("retrieve");
-//     const key = document.getElementById('retrieveKey').value;
-//     let records = window.localStorage.getItem(products);
-//     const div = document.createElement("div");
-//     const infor = document.createTextNode(records);
-//     div.appendChild(infor);
-//     let element = document.getElementById("ayo");
-//     element.appendChild(div);
-// };
-// retrieve();
-// `   <div class="text-dark text-shadow-dark rounded-3 py-5 bible">
-//         <div class="row d-flex justify-content-around">
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/KYtx3N1p/horizonzerodawn-1638924347525.jpg" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Horizon Zero Dawn<br>(Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="h4-mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//             </div>
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/j5Nw5PTR/j-AT7-Hjp-L49-A62b-U7h-LKXJ96b.webp" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Horizon Complete Edition<br> (Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//             </div>
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/sDKBv3kG/MV5-BY2-M5-Y2-I4-Mj-It-NWJk-ZC00-M2-Y1-LThm-NTMt-Nj-Vl-ZDhi-YWRj-ODhh-Xk-Ey-Xk-Fqc-Gde-QXVy-Mz-Y0-MTE3-Nz-U-V1.jpg" class="card-img-top" alt="Meh">
-//                 <div class="card-body mshadow">
-//                   <h5 class="card-title">Horizon The Frozen Wilds<br>(Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//               </div>
-//         </div>
-//     </div>
-//     <div class="text-dark text-shadow-dark rounded-3 py-5 bible">
-//         <div class="row d-flex justify-content-around">
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/pr5JZpKT/MV5-BYm-Q2-Zj-Zi-ZDAt-ZGNh-ZC00-ZDI5-LTkz-MWQt-MTA2-MTFl-Njky-ODc0-Xk-Ey-Xk-Fqc-Gde-QXVy-Mz-Y0-MTE3-Nz-U-V1.jpg" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                     <h5 class="card-title mshadow">Dishonoured<br>(Game)</h5>
-//                     <h3 class="mshadow">•Price•</h3>
-//                     <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//             </div>
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/90DJnxHc/dishonored-5.jpg" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Dishonoured The Return Of Daud<br> (Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//             </div>
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/MTFYVyMN/Definitive-Edition-Cover.webp" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Dishonoured Definitive Edition<br>(Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//               </div>
-//         </div>
+if (ItemNo == "" || Item == "" || RefNo == "" || Price == "" || Qty == ""){
+}else{
+    if(products == []){
+        const list = document.querySelector(localStorage.setItem("#row-wor"));
+        const row = document.createElement(localStorage.setItem("tr"));
 
-//     </div>
-//     <div class="text-dark text-shadow-dark rounded-3 py-5 bible">
-//         <div class="row d-flex justify-content-around">
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/h4J9dQDW/masseffect3-sq-1641945184920.jpg" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Mass Effect<br>(Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//             </div>
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/qMs6s8WY/uh-Wvgrh-Jk-Mk2-Kt-Srmq-XO6qr-Y7nkey-GMO.jpg" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Mass Effect Andromeda<br> (Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//             </div>
-//             <div class="card" style="width: 18rem;">
-//                 <img src="https://i.postimg.cc/Z5JBVWSq/LYkcxj-G6uj-KJJKUk3-Isx-Psg-Am-AVYTu-MB.webp" class="card-img-top" alt="Meh">
-//                 <div class="card-body">
-//                   <h5 class="card-title mshadow">Mass Effect Andromeda Deluxe Upgrade<br>(Game)</h5>
-//                   <h3 class="mshadow">•Price•</h3>
-//                   <h4 class="mshadow">|R500|</h4>
-//                   <a href="../html/checkout.html" class="btn btn-info">Add To Checkout</a>
-//                 </div>
-//               </div>
-//         </div>
+        row.innerHTML = `   <div class="text-dark text-shadow-dark rounded-3 py-5 bible">
+        <div class="row d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/KYtx3N1p/horizonzerodawn-1638924347525.jpg" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Horizon Zero Dawn<br>(Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="h4-mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/j5Nw5PTR/j-AT7-Hjp-L49-A62b-U7h-LKXJ96b.webp" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Horizon Complete Edition<br> (Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/sDKBv3kG/MV5-BY2-M5-Y2-I4-Mj-It-NWJk-ZC00-M2-Y1-LThm-NTMt-Nj-Vl-ZDhi-YWRj-ODhh-Xk-Ey-Xk-Fqc-Gde-QXVy-Mz-Y0-MTE3-Nz-U-V1.jpg" class="card-img-top" alt="Meh">
+                <div class="card-body mshadow">
+                  <h5 class="card-title">Horizon The Frozen Wilds<br>(Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+              </div>
+        </div>
+    </div>
+    <div class="text-dark text-shadow-dark rounded-3 py-5 bible">
+        <div class="row d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/pr5JZpKT/MV5-BYm-Q2-Zj-Zi-ZDAt-ZGNh-ZC00-ZDI5-LTkz-MWQt-MTA2-MTFl-Njky-ODc0-Xk-Ey-Xk-Fqc-Gde-QXVy-Mz-Y0-MTE3-Nz-U-V1.jpg" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                    <h5 class="card-title mshadow">Dishonoured<br>(Game)</h5>
+                    <h3 class="mshadow">•Price•</h3>
+                    <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/90DJnxHc/dishonored-5.jpg" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Dishonoured The Return Of Daud<br> (Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/MTFYVyMN/Definitive-Edition-Cover.webp" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Dishonoured Definitive Edition<br>(Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+              </div>
+        </div>
 
-//     </div>
-// </div>     
-// </div>`
+    </div>
+    <div class="text-dark text-shadow-dark rounded-3 py-5 bible">
+        <div class="row d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/h4J9dQDW/masseffect3-sq-1641945184920.jpg" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Mass Effect<br>(Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/qMs6s8WY/uh-Wvgrh-Jk-Mk2-Kt-Srmq-XO6qr-Y7nkey-GMO.jpg" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Mass Effect Andromeda<br> (Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="https://i.postimg.cc/Z5JBVWSq/LYkcxj-G6uj-KJJKUk3-Isx-Psg-Am-AVYTu-MB.webp" class="card-img-top" alt="Meh">
+                <div class="card-body">
+                  <h5 class="card-title mshadow">Mass Effect Andromeda Deluxe Upgrade<br>(Game)</h5>
+                  <h3 class="mshadow">•Price•</h3>
+                  <h4 class="mshadow">|R500|</h4>
+                  <a href="../html/checkout.html" class="btn btn-info" id="view-product">Add To Checkout</a>
+                </div>
+              </div>
+        </div>
+
+    </div>
+</div>     
+</div>`;
+        list.appendChild(row);
+    }else{
+        products.children[0].textContent = ItemNo;
+        products.children[1].textContent = Item;
+        products.children[2].textContent = Price;
+        products.children[3].textContent = Qty;
+        products.children[4].textContent = Total;
+    }
+    updatePage();
+};
+
+
 ;
 // Local Storage //
-// document.getElementById('view-product').addEventListener("click", function(){
-//     window.localStorage.Item('data', 'products');
-// });
+document.getElementById('view-product').addEventListener("click", function(){
+    window.localStorage.Item('data', 'products');
+});
 // Store
 // localStorage.setItem("product", "products");
 // Local Storage //
